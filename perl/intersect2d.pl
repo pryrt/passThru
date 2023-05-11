db7fe8e44cc932ba40b9ca4fb4a8a0d2b7da0f88
PeterCJ <pryrtcode@pryrt.com>
2023-05-11 10:42:09 -0700

    converted intersect2d.pl into the test suite for the module version, and added in the test case of up-vs-diagonal which was what found the original bug

--- perl/intersect2d.pl
+++ perl/intersect2d.pl
@@ -6,18 +6,27 @@ use Carp;
 use FindBin;
 use lib "$FindBin::Bin/lib";
 use Math::Vector::Real::Intersect;
+
+# convert this to the test suite
+use Test::More tests => 6;
 $| = 1;
 
 eval {
     my $space1 = V(1,2,0);
     my $space2 = V(3,4,0);
-    print "space: $space1 x $space2 = ", $space1 x $space2;
+    my $got = $space1 x $space2;
+    my $exp = V(0, 0, -2);
+    is_deeply $got, $exp, "space";
+    note sprintf "\t\tspace: %s x %s = %s vs %s", $space1 , $space2, $got, $exp;
     1;
 } or warn $@;
 eval {
     my $plane1 = V(1,2);
     my $plane2 = V(3,4);
-    print "plane: $plane1 x $plane2 = ", $plane1 x $plane2;
+    my $got = $plane1 x $plane2;
+    my $exp = -2;
+    is_deeply $got, $exp, "plane";
+    note sprintf "\t\tplane: %s x %s = %s vs %s", $plane1 , $plane2, $got, $exp;
     1;
 } or warn $@;
 
@@ -26,7 +35,10 @@ eval {
     my $v2 = V(1,1);
     my $d1 = V(1,0);
     my $d2 = V(0,-1);
-    print "intersect_lines($v1 @ $d1, $v2 @ $d2) = ", intersect_lines($v1,$d1, $v2,$d2);
+    my $got = intersect_lines($v1,$d1, $v2,$d2);
+    my $exp = V(1,0);
+    is_deeply $got, $exp, "intersect_lines: simple";
+    note sprintf "\t\tintersect_lines(%s @ %s, %s @ %s) = %s vs %s", $v1 , $d1, $v2 , $d2, $got, $exp;
     #                               .   (1,1) @ (0,-1)
     #                               |
     #                               V
@@ -41,12 +53,33 @@ eval {
     1;
 } or warn $@;
 
+eval {
+    # this test case found a bug in the original code
+    my $sq = sqrt(0.5);
+    my $v1 = V(1,0);
+    my $v2 = V($sq,$sq);
+    my $d1 = V(0,1);
+    my $d2 = -V(-$sq,$sq);
+    my $got = intersect_lines($v1,$d1, $v2,$d2);
+    my $exp = V(1,sqrt(2)-1);
+    is_deeply $got, $exp, "intersect_lines: test case up-vs-diagonal";
+    note sprintf "\t\tintersect_lines(%s @ %s, %s @ %s) = %s vs %s: test case that found bug in original code", $v1 , $d1, $v2 , $d2, $got, $exp;
+    1;
+} or warn $@;
+
 
 eval {
     my ($c1,$r1) = (V(0,0), 3.14159);
     my ($c2,$r2) = (V(3,4), 2.71828);
-    print "EVAL intersect_circles($c1,$r1,$c2,$r2) = ", intersect_circles($c1,$r1,$c2,$r2);
+    my $got = intersect_circles($c1,$r1,$c2,$r2);
+    my $exp = V(0.430890340295233, 3.11189994099108);
+    is_deeply $got, $exp, "intersect_circles: positive polarity";
+    note sprintf "\t\tintersect_circles(%s,%s,%s,%s) = %s vs %s", $c1,$r1,$c2,$r2, $got, $exp;
+
     $r2 *= -1;
-    print "EVAL intersect_circles($c1,$r1,$c2,$r2) = ", intersect_circles($c1,$r1,$c2,$r2);
+    $got = intersect_circles($c1,$r1,$c2,$r2);
+    $exp = V(2.86677464806877, 1.28498671016092);
+    is_deeply $got, $exp, "intersect_circles: negative polarity";
+    note sprintf "\t\tintersect_circles(%s,%s,%s,%s) = %s vs %s", $c1,$r1,$c2,$r2, $got, $exp;
     1;
 } or warn $@;
