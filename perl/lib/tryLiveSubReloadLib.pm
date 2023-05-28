49a764633a7a15b685bcee1792248e48569c2a41
PeterCJ <pryrtcode@pryrt.com>
2023-05-28 16:51:56 -0700

    tryLiveSubReload: starting to figure out how they work together

---
+++ perl/lib/tryLiveSubReloadLib.pm
@@ -0,0 +1,36 @@
+package tryLiveSubReloadLib;
+use 5.012; # //, strict, say
+use warnings;
+
+=begin ideas
+
+There are two challenges that I want to get working:
+
+1. If script uses this from the get-go, do some initialization...
+   but if this then runs `do` on the script, it won't re-run/re-initialize
+   - since %INC will have been updated to have this module, this module will _not_ reload, so that means it's safe
+   - however, if the script does anythig other than define subroutines, it will need to be protected by an unless(caller(0)) condition
+     (and `if(caller(0)){exit;}` will actually exit the top-level script, not just the `do`.)
+2. if the calling-script defines a sub, will the module see the update to that sub
+   _after_ the `do` is run?
+   - need the `no warnings 'redefine` to go in the script, _not_ in the library; annoying
+
+=cut
+
+my $c = 0;
+while(my @c = caller($c)) {
+    printf STDERR "tLSRL:  #%d => (%s)\n", $c, join ",", map { $_//"<undef>"} @c;
+    ++$c;
+}
+
+sub recurse
+{
+    my ($scriptPath) = @_;
+    no warnings qw(redefine);
+    printf STDERR "tLSRL:  before first do('%s')\n", $scriptPath;
+    eval { do $scriptPath; 1; } or do { print "tLSRL:  eval says \$@ = $@\n"; };
+    printf STDERR "tLSRL:  after  first do('%s')\n", $scriptPath;
+    eval { do $scriptPath; 1; } or do { print "tLSRL:  eval says \$@ = $@\n"; };
+    printf STDERR "tLSRL:  after  second do('%s')\n", $scriptPath;
+}
+1;
---
+++ perl/tryLiveSubReload.pl
@@ -0,0 +1,36 @@
+#!perl
+
+use 5.012; # //, strict, say
+use warnings;
+use FindBin;
+use lib "$FindBin::Bin/lib";
+BEGIN { printf STDERR "script: before use tryLiveSubReloadLib: INC has(%s)\n", join '; ', map {"$_ => $INC{$_}"} grep {/tryLiveSubReload/} sort keys (%INC); }
+use tryLiveSubReloadLib;
+BEGIN { printf STDERR "script: after  use tryLiveSubReloadLib: INC has(%s)\n", join '; ', map {"$_ => $INC{$_}"} grep {/tryLiveSubReload/} sort keys (%INC); }
+
+print STDERR "script: before caller(n) check\n";
+my $c = 0;
+while(my @c = caller($c)) {
+    printf STDERR "script: #%d => (%s)\n", $c, join ",", map { $_//"<undef>"} @c;
+    ++$c;
+}
+print STDERR "script: after caller(n) check\n";
+
+if(caller(0)) {
+    print STDERR "script: prevent infinte recursion when this script is read again\n";
+    #exit; # cannot use exit, because that exits the top-level script
+} else {
+    print STDERR "script: before tryLiveSubReloadLib::recurse()\n";
+    my $source = $FindBin::Script;
+    unless($source =~ m{/}) { $source = './' . $FindBin::Script; }
+    tryLiveSubReloadLib::recurse($source);
+    print STDERR "script: after  tryLiveSubReloadLib::recurse()\n";
+}
+
+{
+    no warnings 'redefine';
+    sub setup { print STDERR "this is the setup function\n" }
+    sub loopy { print STDERR "this is the loopy function\n" }
+}
+
+1;
