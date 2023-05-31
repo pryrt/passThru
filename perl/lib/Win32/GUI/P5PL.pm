39e820dcb9d5c4ec1538522b9e80cf3b1e457fb7
PeterCJ <pryrtcode@pryrt.com>
2023-05-31 07:46:14 -0700

    add some docs to ::P5PL, including some ideas as to things I want to work on soon

--- perl/lib/Win32/GUI/P5PL.pm
+++ perl/lib/Win32/GUI/P5PL.pm
@@ -10,17 +10,106 @@ use GD 2.77;
 
 our @EXPORT = qw/createCanvas background/;
 
+=pod
+
+=encoding utf8
+
+=head1 NAME
+
+Win32::GUI::P5PL - GUI application inspired by p5*js
+
+=head1 SYNOPSIS
+
+    # sketch.p5.pl
+    #!perl
+    use 5.014; # //, strict, say, s//r
+    use warnings;   no warnings 'redefine';
+    use lib './lib';
+    use Win32::GUI::P5PL;
+    if(!caller){Win32::GUI::P5PL::App->launch();}
+
+    sub setup() {
+      createCanvas(800, 400);
+    }
+
+    sub draw() {
+      background(220);
+    }
+
+=head1 DESCRIPTION
+
+Provides an environment similar to the L<p5*js editor|https://editor.p5js.org/> ,
+but currently focuses on just the CANVAS-related aspects: the setup and draw loop
+for the image, and the ability to react to live changes in the script.
+
+It does not implement the rest of the DOM features of p5*js, nor the actual editor
+panel (though the latter may eventually be implemented).
+
+The SYNOPSIS gives the basic structure that your script will use: you define
+a C<setup()> and C<draw()> function, and when you hit PLAY, the app will run
+C<setup()> and then loop on C<draw()> (as appropriate).
+
+=head2 Live Changes
+
+It seems, while watching Coding Train videos, that if you edit the script while
+it's running, the next draw loop will immediately incorporate the changes saved
+in the file.  I wanted to replicate that, so essentially every loop, I am re-running
+the script, in case <setup()> and C<draw()> were re-defined since the last save.
+Because of that, those functions are redefined every loop, so we need to turn off
+those warnings in the script.
+
+As a result of this philosophy, your script is rerun frequently, so it should not
+"do" anything (have any code not in functions) other than the initial C<use>
+statements, and the C<if(!caller){Win32::GUI::P5PL::App->launch();}> boilerplate.
+
+Improvements I want to make in order for the Live Changes to be more seamless:
+
+=over
+
+=item * ☐ Only re-run the script when the file has changed since the last loop.
+
+I<idea: See L<-X>'s C<-M> modification time check.>
+
+=item * ☐ Figure out how to temporarily inject the C<no warnings 'redefine';>
+so that it doesn't have to be in the script.
+
+I<idea: look at how L<Modern::Perl> injects warnings/strict/features into the calling script>.
+
+=item * ☐ Automatically do the equivalent of the C<if(!caller)...> call,
+so that it doesn't have to be in the script.
+
+My initial experiments tried to implement that: while the C<use> statement will
+only run the C<P5PL.pm> itself once when the sketch is first run, the
+C<P5PL::import()> appears to run every time.  That was originally my downfall,
+but might also end up being what I can use to track whether it's the first load
+or not.  (Do a import-counting state variable, and only run the GUI on the first time)
+
+=back
+
+
+=head1 REFERENCE
+
+=cut
+
+=head2 Rendering
+
+=over
+
+=item createCavas
+
+=item resizeCanvas
+
+    createCanvas($w,$h);
+
+Either function initializes the canvas width and height, and starts a new underlying image object.
+
+(I couldn't see an effective difference in terms of features I'm implementing, so they are implemented
+with the same underlying code in my library.  I do not claim this is true of the original p5*js.)
+
+=cut
+
 my $img;
 sub _pushImg { Win32::GUI::P5PL::App::updateBitmap(MIME::Base64::encode($img->bmp())) }
-
-# the p5*js style functions get defined here
-
-# TODO:
-#   I want all the GD::Image stuff in P5PL, but the Win32::GUI stuff in App
-#   - ::App needs a way to requestResize(w,h), and I would call that App::requestResize from createCanvas()
-#   - ::App needs an setBmpFromB64, which takes as its input MIME::Base64::encode( $img->bmp() ), where $img = GD::Image
-#   - these P5PL functions will be what changes pixels in the GD::Image, and then they'll push the bmp to the ::App::setBmpFromB64()
-
 sub createCanvas
 {
     my ($w, $h) = @_;
@@ -31,6 +120,14 @@ sub createCanvas
 }
 
 *resizeCanvas = \&createCanvas;
+
+=item background
+
+C<background()> will set the background color and fill the whole image with that color.
+When used in C<setup()>, it initializes it once.  When used in C<draw()>, it's basically
+a clear-screen command.
+
+=cut
 
 sub background
 {
@@ -59,5 +156,45 @@ sub background
     $img->fill(0,0, $img->colorResolve($r,$g,$b));  # TODO: incorporate alpha if available
     _pushImg();
 }
+
+=back
+
+=head2 Structure
+
+=over
+
+=item noLoop()
+
+=item loop()
+
+=item isLooping()
+
+TBD: Control the draw-loop mechanism.  By default, C<isLooping()> is true,
+and C<draw()> will be called repeatedly.  Running C<noLoop()> will stop
+the C<draw()> loop, and C<loop()> will re-start it.
+
+B<TODO>: ☐ need to figure out how to propagate this to the actual ::App-based loop mechanism.
+
+=cut
+
+my $_is_looping;
+sub isLooping { $_is_looping }
+sub noLoop { $_is_looping = 0 }
+sub loop { $_is_looping = 1 }
+
+=item push()
+
+=item pop()
+
+TBD: There is apparently a stack of transform/color/stroke settings which can
+be pushed and popped, to be able to easily handle sets of drawing conditions.
+
+=back
+
+=head1 AUTHOR
+
+Copyright (C) Peter C. Jones, 2023
+
+=cut
 
 1;
--- perl/sketch.p5.pl
+++ perl/sketch.p5.pl
@@ -1,6 +1,6 @@
 #!perl
 use 5.014; # //, strict, say, s//r
-use warnings;   no warnings 'redefine';
+use warnings; no warnings 'redefine';
 use lib './lib';
 use Win32::GUI::P5PL;
 if(!caller){Win32::GUI::P5PL::App->launch();}
