19e9b54111668c27c9698339d7e3708ff992fadd
PeterCJ <pryrtcode@pryrt.com>
2023-07-22 10:32:45 -0700

    figuring out how to run the activation function and slope

--- perl/toy.nn/examples/xor.pl
+++ perl/toy.nn/examples/xor.pl
@@ -10,12 +10,12 @@ use ToyNN::PerceptronNetwork;
 my $w = pdl [[ 1.0 , 1.0 ],[ 1.0 , 1.0 ]];
 my $b = pdl([ -1.5, -0.5 ])-> transpose;
 my $xcols = pdl [[ 0, 0, 1, 1],[ 0, 1, 0, 1]];
-print "manual xor = w * xcols + b => ",my $manual_xor = $w x $xcols + $b;
+#print "manual xor = w * xcols + b => ",my $manual_xor = $w x $xcols + $b;
 
 # debugging
 use Data::Dump;
 my $layer = ToyNN::PerceptronLayer::->new(2, 2);
-print "Layer.Bias    => ", $layer->B;
-print "Layer.Weights => ", $layer->W;
-print "Layer.Bias    => ", $layer->biases;
-print "Layer.Weights => ", $layer->weights;
+printf "%+5.1f => %+9.6f %+9.6f\n", $_, $layer->{fn}->($_), $layer->{df}->($_) for map {$_} -10 .. 10;exit;
+#print "Layer.Bias    => ", $layer->B;
+#print "Layer.Weights => ", $layer->W;
+$layer->feedforward( $xcols->slice('3,:') );    # third column only
--- perl/toy.nn/lib/ToyNN/PerceptronLayer.pm
+++ perl/toy.nn/lib/ToyNN/PerceptronLayer.pm
@@ -3,14 +3,23 @@ use 5.014; # //, strict, say, s///r
 use warnings;
 use PDL;
 
+my $this;
+thread_define('square(a();[o]b())', over { $_[1] .= $_[0] ** 2 });
+thread_define('other(a();[o]b())',  over { printf "other(%s,%s)\n", $this->{fn}, $_[0]; $_[1] .= $this->{fn}->($_[0]); });
+thread_define('fn(a();[o]b())',  over { $_[1] .= $this->fn($_[0]) });
+#thread_define('df(a();[o]b())', over { $_[1] .= $this->df($_[0]) });
+sub prepare_broadcast { $this = $_[0] }
+
 sub new
 {
     my ($class, $nIn, $nOut) = @_;
 
     my $self = bless {
         n => $nOut,
-        W => random($nIn, $nOut),           # one column per input, one row per output
-        B => random($nOut)->transpose(),    # one column, one row per output
+        W => sequence($nIn, $nOut)/10,      #random($nIn, $nOut),           # one column per input, one row per output
+        B => sequence($nOut)->transpose/10, #random($nOut)->transpose(),    # one column, one row per output
+        fn => \&sigmoid,
+        df => \&dsigmoid,
     }, $class;
 
     return $self;
@@ -18,5 +27,32 @@ sub new
 
 sub W { $_[0]->{W} }; sub weights { $_[0]->{W} };
 sub B { $_[0]->{B} }; sub biases  { $_[0]->{B} };
+
+sub feedforward
+{
+    my ($self, $inputs) = @_;
+    my $sums = $self->W x $inputs  + $self->B;
+    print "ff(weighted sum) => ", $sums;
+    $self->prepare_broadcast;
+    #fn($sums, my $activated = PDL->null);
+    #square($sums, (my $out = PDL->null));
+    other($sums, (my $out = PDL->null));
+    print $out;
+}
+
+sub sigmoid($)
+{
+    my ($x) = @_;
+    return 1 / (1 + exp(-$x));
+}
+
+sub dsigmoid($)
+{
+    my ($x) = @_;
+    my $s = sigmoid($x);
+    return $s * (1 - $s);
+}
+
+
 
 1;
