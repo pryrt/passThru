b5a269f6aa8a80f1c26df5276a54f36433dff7d3
PeterCJ <pryrtcode@pryrt.com>
2023-07-23 16:58:44 -0700

    PerceptronNetwork can feedforward for at least a single layer

--- perl/toy.nn/examples/xor.pl
+++ perl/toy.nn/examples/xor.pl
@@ -1,4 +1,5 @@
 #!perl -l
+# now moving from a standalone layer to a PerceptronNetwork that has only a single layer
 
 use 5.014; # //, strict, say, s///r
 use warnings;
@@ -7,9 +8,6 @@ use lib "${FindBin::Bin}/../lib";
 use PDL;
 use ToyNN::PerceptronNetwork;
 $| = 1;
-
-
-# standalone layer
 
 # define the inputs
 my $X = pdl [[ 0, 0, 1, 1],[ 0, 1, 0, 1]];
@@ -21,12 +19,24 @@ my $TARGET = pdl
     [map {ToyNN::PerceptronLayer::sigmoid($_)} -5,5,5,15]
 ];
 
-# create a standalone layer
-my $layer = ToyNN::PerceptronLayer::->new(2, 2);
-$layer->{W} .= 1;   # set all elements to 1
-$layer->{B} .= 1;   # set all elements to 1
-$layer->set_learning_rate(1);
+# create a network with just one layer
+my $network = ToyNN::PerceptronNetwork::->new(2, 2);
+for my $layer ( @{ $network->{layers} }) {
+    # for testing purposes, guarantee that all weights and biases are 1
+    $layer->{W} .= 1;   # set all elements to 1
+    $layer->{B} .= 1;   # set all elements to 1
+    $layer->set_learning_rate(1);
+    print $layer->W, $layer->B;
+}
 
+my $output = $network->feedforward($X);
+print "Output => ", $output;
+
+# next, do a single training epoch -- start without any of the complications of multiple layers, but add that in later
+
+__END__
+
+# train 100 epochs
 for(1..100) {
     my $Q = $layer->feedforward($X);
     my $E = $TARGET - $Q;
--- perl/toy.nn/lib/ToyNN/PerceptronNetwork.pm
+++ perl/toy.nn/lib/ToyNN/PerceptronNetwork.pm
@@ -4,4 +4,32 @@ use warnings;
 use PDL;
 use ToyNN::PerceptronLayer;
 
+
+sub new
+{
+    my ($class, $nIn, @nOutPerLayer) = @_;
+    my $self = bless {
+        layers => [],
+    }, $class;
+
+    for my $n (@nOutPerLayer) {
+        push @{$self->{layers}}, ToyNN::PerceptronLayer::->new($nIn, $n);
+        $nIn = $n; # the next layer will have the number of its inputs be the same as the number of outputs from this layer
+    }
+
+    return $self;
+}
+
+sub feedforward
+{
+    my ($self, $X) = @_;
+
+    my $Y;
+    for my $layer (@{ $self->{layers} }) {
+        $Y = $layer->feedforward($X);
+        $X = $Y; # the next layer will have its inputs be the outputs from this layer
+    }
+    return $Y;
+}
+
 1;
