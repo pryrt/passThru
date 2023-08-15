4d9b1846a078b5da73226f2454c1f087aff9274f
pryrt <pryrtcode@pryrt.com>
2023-08-15 10:53:30 -0700

    steps 9b-14
    9b: else no capacity, so subdivide
    
    10-13. implement subdivide
    
    14. after subdividing, insert point into each of the four quadrants (will be fixed in 15)

--- perl/quadTree/lib/myQuadTree.pm
+++ perl/quadTree/lib/myQuadTree.pm
@@ -30,8 +30,12 @@ sub boundary { return $_[0]->{boundary} }
 sub capacity { return $_[0]->{capacity} }
 sub items    { return $_[0]->{items} }
 
-# lvalue: simple getter/setter
+# lvalue: simple getter/setters
 sub divided : lvalue  { $_[0]->{divided} }
+sub northwest : lvalue { $_[0]->{northwest} }
+sub northeast : lvalue { $_[0]->{northeast} }
+sub southwest : lvalue { $_[0]->{southwest} }
+sub southeast : lvalue { $_[0]->{southeast} }
 
 # step 9 required a count of the points/items in the current tree node; I want to abstract that
 sub countItems
@@ -46,7 +50,27 @@ sub addItemAtPoint
     if($self->countItems < $self->capacity) {
         my $pointItem = Item($item, $ptx, $pty);
         push @{$self->items}, $pointItem;
+        return 1;
+    } else {
+        $self->divided or $self->subdivide();
+        # now that it's subdivided, let each of the four quadrants decide whether to accept
+        $self->northwest->addItemAtPoint($item, $ptx, $pty);   # at step 14, the point will incorrectly go in all four quadrants
+        $self->northeast->addItemAtPoint($item, $ptx, $pty);   # at step 14, the point will incorrectly go in all four quadrants
+        $self->southwest->addItemAtPoint($item, $ptx, $pty);   # at step 14, the point will incorrectly go in all four quadrants
+        $self->southeast->addItemAtPoint($item, $ptx, $pty);   # at step 14, the point will incorrectly go in all four quadrants
     }
+}
+
+sub subdivide
+{
+    my ($self) = @_;
+    my ($cx,$cy,$rx,$ry) = map { $self->boundary()->{$_} } qw/cx cy rx ry/;
+    my $cap = $self->capacity;
+    my $nw = Rectangle($cx-$rx/2, $cy+$ry/2, $rx/2, $ry/2); $self->northwest = myQuadTree($nw, $cap);
+    my $ne = Rectangle($cx+$rx/2, $cy+$ry/2, $rx/2, $ry/2); $self->northeast = myQuadTree($ne, $cap);
+    my $sw = Rectangle($cx-$rx/2, $cy-$ry/2, $rx/2, $ry/2); $self->southwest = myQuadTree($sw, $cap);
+    my $se = Rectangle($cx+$rx/2, $cy-$ry/2, $rx/2, $ry/2); $self->southeast = myQuadTree($se, $cap);
+    $self->divided = 1;
 }
 
 1;
--- perl/quadTree/tryMyQT.pl
+++ perl/quadTree/tryMyQT.pl
@@ -16,5 +16,58 @@ use myQuadTree;
 
 my $outer = myQuadTree::Rectangle(0,0,1,1); # step4
 my $qtree = myQuadTree($outer, 4);          # step4
-$qtree->addItemAtPoint(\"dummyItem1", sqrt(.5), sqrt(.5));  # step5
+$qtree->addItemAtPoint(\"itm0", 0.5, 0.5);  # step5
+$qtree->addItemAtPoint(\"itm$_", int(-10+rand 20)/10, int(-10 + rand 20)/10) for 1..4;  # step9 : trigger subdivide
 dd $qtree;
+
+__END__
+
+do {
+  my $a = bless({
+    boundary  => bless({ cx => 0, cy => 0, rx => 1, ry => 1, type => "rectangle" }, "myQuadTree::Region"),
+    capacity  => 4,
+    divided   => 1,
+    items     => [
+                   bless({ cx => 0.5, cy => 0.5, myItem => \"dummyItem0", rx => 0, ry => 0, type => "point" }, "myQuadTree::Region"),
+                   bless({ cx => -0.08, cy => -0.71, myItem => \"dummyItem1", rx => 0, ry => 0, type => "point", }, "myQuadTree::Region"),
+                   bless({ cx => -0.99, cy => -0.02, myItem => \"dummyItem2", rx => 0, ry => 0, type => "point", }, "myQuadTree::Region"),
+                   bless({ cx => 0.53, cy => -0.24, myItem => \"dummyItem3", rx => 0, ry => 0, type => "point" }, "myQuadTree::Region"),
+                 ],
+    northeast => bless({
+                   boundary => bless({ cx => 0.5, cy => 0.5, rx => 0.5, ry => 0.5, type => "rectangle" }, "myQuadTree::Region"),
+                   capacity => 4,
+                   divided  => undef,
+                   items    => [
+                                 bless({ cx => 0.26, cy => 0.57, myItem => \"dummyItem4", rx => 0, ry => 0, type => "point" }, "myQuadTree::Region"),
+                               ],
+                 }, "myQuadTree"),
+    northwest => bless({
+                   boundary => bless({ cx => -0.5, cy => 0.5, rx => 0.5, ry => 0.5, type => "rectangle" }, "myQuadTree::Region"),
+                   capacity => 4,
+                   divided  => undef,
+                   items    => [
+                                 bless({ cx => 0.26, cy => 0.57, myItem => 'fix', rx => 0, ry => 0, type => "point" }, "myQuadTree::Region"),
+                               ],
+                 }, "myQuadTree"),
+    southeast => bless({
+                   boundary => bless({ cx => 0.5, cy => -0.5, rx => 0.5, ry => 0.5, type => "rectangle" }, "myQuadTree::Region"),
+                   capacity => 4,
+                   divided  => undef,
+                   items    => [
+                                 bless({ cx => 0.26, cy => 0.57, myItem => 'fix', rx => 0, ry => 0, type => "point" }, "myQuadTree::Region"),
+                               ],
+                 }, "myQuadTree"),
+    southwest => bless({
+                   boundary => bless({ cx => -0.5, cy => -0.5, rx => 0.5, ry => 0.5, type => "rectangle" }, "myQuadTree::Region"),
+                   capacity => 4,
+                   divided  => undef,
+                   items    => [
+                                 bless({ cx => 0.26, cy => 0.57, myItem => 'fix', rx => 0, ry => 0, type => "point" }, "myQuadTree::Region"),
+                               ],
+                 }, "myQuadTree"),
+  }, "myQuadTree");
+  $a->{northwest}{items}[0]{myItem} = \${$a->{northeast}{items}[0]{myItem}};
+  $a->{southeast}{items}[0]{myItem} = \${$a->{northeast}{items}[0]{myItem}};
+  $a->{southwest}{items}[0]{myItem} = \${$a->{northeast}{items}[0]{myItem}};
+  $a;
+}
