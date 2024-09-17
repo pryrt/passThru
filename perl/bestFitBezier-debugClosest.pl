a74dd3a80ad6e8ce5ee7741cc1cc668a33fcca1b
pryrt <pryrtcode@pryrt.com>
2024-09-17 10:45:24 -0700

    now that it's working, add in the improvement iteration and the final graph as well -- still looking good

--- perl/bestFitBezier-debugClosest.pl
+++ perl/bestFitBezier-debugClosest.pl
@@ -12,7 +12,7 @@ use GD;
 
 BEGIN { $| = 1; }
 
-my $im = GD::Image::->new(400,400,1);
+my $im = GD::Image::->new(400*1,400,1);
 my $c_bg = $im->colorAllocate(51,51,51);
 my $c_plot = $im->colorAllocate(191,191,191);
 my $c_samp = $im->colorAllocate(255,0,0);
@@ -63,6 +63,52 @@ for my $s (@samples) {
     printf "s:[%+9.6f,%+9.6f] -> B(t:%+9.6f) = [%+9.6f,%+9.6f] => dsq:%.6f\n", @$s, $t, @$v, $d;
     $im->line(v2px($s), v2px($v), $c_close);
     $im->filledEllipse(v2px($v), 4,4, $c_close);
+}
+
+
+# Maybe the visualization I really want is just to plot sum(dsq) as
+#   color, for each of the handful of <p2x,p1y> pairs I pick.
+my @keep = ('Inf', undef, undef, undef);
+my $max = 50;
+for my $j ( 0 .. $max ) {
+    my $p1x = $j / $max;
+    for my $i ( 0 .. $max ) {
+        my $p2y = $i / $max;
+        my $g = CubicBezier($P0, V($p1x,$P0->[1]), V($P3->[0],$p2y), $P3);
+        my $sum_dsq = 0;
+        for my $s (@samples) {
+            my ($t, $d, $v) = $g->closestToPoint($s);
+            $sum_dsq += $d;
+        }
+        if($sum_dsq < $keep[0]) {
+            @keep = ($sum_dsq, $p1x, $p2y, $g);
+        }
+        #printf "p1x=%+6.3f, p2y=%+6.3f -> sum(dsq) = %+.6f\n", $p1x, $p2y, $sum_dsq;
+        my $m = 255-int(255*$sum_dsq*2.5);
+        my $c = $im->colorResolve(0,$m,$m);
+        my $ppt = V($p1x,$p2y);
+        #printf "[%+6.3f,%+6.3f] 0x%06X\n", @$ppt, $c;
+        $im->setPixel( v2px(V(1,0)+$ppt), $c);
+    }
+}
+print "final => @keep\n";
+
+# final guess
+$guess = $keep[3];
+printf "FINAL BEZ(%s,%s,%s,%s)\n", $guess->{p0}, $guess->{p1}, $guess->{p2}, $guess->{p3};
+my $Q = V(0,0);
+for (1 .. 10) {
+    my $t = $_/10;
+    my $prev = ($_-1)/10;
+    my $v = $guess->B($t);
+    my $p = $guess->B($prev);
+    $im->line(v2px($Q+$p), v2px($Q+$v), $c_plot);
+}
+for my $s (@samples) {
+    $im->filledEllipse(v2px($Q+$s), 8,8, $c_samp);
+    my ($t, $d, $v) = $guess->closestToPoint($s);
+    printf "s:[%+6.3f,%+6.3f] -> B(t:%+6.3f) = [%+6.3f,%+6.3f] => dsq:%.3f\n", @$s, $t, @$v, $d;
+    $im->filledEllipse(v2px($Q+$v), 4,4, $c_close);
 }
 
 
